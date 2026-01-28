c8477bcab246c2c41877bb16da0a3cece84e60a8
Dishambha Awasthi <dishambhaawasthi0131@gmail.com>
2026-01-28 15:47:26 +0530

    Use environment-based API base URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,9 @@ import "./App.css";
 import TaglineSection from "./TaglineSection";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: process.env.REACT_APP_API_URL,
 });
+
 
 function App() {
   const [products, setProducts] = useState([]);
