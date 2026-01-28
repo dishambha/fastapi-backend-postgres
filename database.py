40f3893b1738effb4fac35b288b48e6150f579cc
Dishambha Awasthi <dishambhaawasthi0131@gmail.com>
2026-01-28 15:19:18 +0530

    Configure PostgreSQL connection using DATABASE_URL

--- database.py
+++ database.py
@@ -1,6 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-db_url = "postgresql://postgres:dishambha@localhost:5432/fastapi_db"
-engine = create_engine(db_url)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set")
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
